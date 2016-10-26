edac580be91a987bbb23ca19d7dfda1572b0a76e
Raony Guimarães <raonyguimaraes@gmail.com>
2016-10-26 08:43:19 -0200

    fix static files

--- mendelmd_source/mendelmd/settings.py
+++ mendelmd_source/mendelmd/settings.py
@@ -148,7 +148,7 @@ STATIC_URL = '/static/'
 
 # STATIC_ROOT = os.path.join(BASE_DIR,'static/')
 
-STATIC_ROOT = ''
+STATIC_ROOT = '/var/www/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
