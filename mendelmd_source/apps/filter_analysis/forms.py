e7e2112c9d41f8c0c2051a2cd14e27531043dabb
Raony Guimaraes <raony.guimaraes@genomicsmed.ie>
2017-03-29 22:13:38 +0100

    small fix

--- mendelmd_source/apps/filter_analysis/forms.py
+++ mendelmd_source/apps/filter_analysis/forms.py
@@ -215,8 +215,15 @@ class FilterAnalysisForm(forms.Form):
             self.fields['exclude_individuals'] = forms.ModelMultipleChoiceField(queryset=Individual.objects.filter(user=None).order_by('id'), required=False, label='INDIVIDUALS')
         else:
             print('user', user)
-            self.fields['individuals'] = forms.ModelMultipleChoiceField(queryset=Individual.objects.filter(user=user).order_by('id'), required=False, label='INDIVIDUALS')
-            self.fields['exclude_individuals'] = forms.ModelMultipleChoiceField(queryset=Individual.objects.filter(user=user).order_by('id'), required=False, label='INDIVIDUALS')
+            if user.is_superuser():
+                
+                self.fields['individuals'] = forms.ModelMultipleChoiceField(queryset=Individual.objects.all().order_by('id'), required=False, label='INDIVIDUALS')
+                self.fields['exclude_individuals'] = forms.ModelMultipleChoiceField(queryset=Individual.objects.all().order_by('id'), required=False, label='INDIVIDUALS')
+            
+            else:
+
+                self.fields['individuals'] = forms.ModelMultipleChoiceField(queryset=Individual.objects.filter(user=user).order_by('id'), required=False, label='INDIVIDUALS')
+                self.fields['exclude_individuals'] = forms.ModelMultipleChoiceField(queryset=Individual.objects.filter(user=user).order_by('id'), required=False, label='INDIVIDUALS')
             
         # self.fields['sift'].widget.attrs['readonly'] = True
         # self.fields['polyphen'].widget.attrs['readonly'] = True
