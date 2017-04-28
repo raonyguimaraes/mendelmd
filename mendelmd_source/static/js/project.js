82c19d1e8461788ac278dafc312f1637ef1846a4
Raony Guimaraes <raony.guimaraes@genomicsmed.ie>
2017-04-28 17:20:19 +0100

    fix checkbxes

--- mendelmd_source/static/js/project.js
+++ mendelmd_source/static/js/project.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+$("#select_all").change(function(){
+  $(".individuals").prop("checked", $(this).prop("checked"));
+  });
+});
+
+
 $(document).ready(function() {
 
   $( "#slider-range" ).slider({
