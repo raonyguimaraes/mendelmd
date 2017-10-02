26924a25b34610dc4e85cccc65cb45c532c5008c
Raony Guimaraes <raonyguimaraes@gmail.com>
2017-10-02 07:44:21 +0000

    update celery to v4

--- mendelmd/celery.py
+++ mendelmd/celery.py
@@ -1,7 +1,7 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from django.conf import settings
+
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mendelmd.settings')
 
@@ -11,18 +11,13 @@ app = Celery('mendelmd')
 # the configuration object to child processes.
 # - namespace='CELERY' means all celery-related configuration keys
 #   should have a `CELERY_` prefix.
-app.config_from_object('django.conf:settings')
+app.config_from_object('django.conf:settings', namespace='CELERY')
 
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
-# app.config_from_object({
-#     'BROKER_URL': 'django://',
-#     'CELERY_RESULT_BACKEND': 'db+sqlite:///mendelmd.sqlite3',
-#     'CELERYD_POOL_RESTARTS': True,  # Required for /worker/pool/restart API
-# })
 # Load task modules from all registered Django app configs.
-# app.autodiscover_tasks()
+app.autodiscover_tasks()
 
 
 @app.task(bind=True)
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
+
