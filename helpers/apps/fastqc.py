1baef37cb4861a644a473006623da386e048c822
Raony Guimaraes <raonyguimaraes@gmail.com>
2018-02-18 16:48:47 +0000

    checkpoint

--- helpers/apps/fastqc.py
+++ helpers/apps/fastqc.py
@@ -1,11 +1,31 @@
+import argparse
 from subprocess import run
+import sys
+
+sys.path.append('$HOME/miniconda/bin')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('args', nargs='?')
+parser.add_argument('-i', '--input', nargs='+', help='input')
+args = parser.parse_args()
+print(args)
 
 class FASTQC():
-	def __init__(self):
-		pass
-	def install(self):
-		command = 'conda install fastqc'
-		run(command, shell=True)
-	def run(self, fastq):
-		command = 'fastqc {} -o output/'.format(fastq)
-		run(command, shell=True)
+    def __init__(self):
+        pass
+    def install(self):
+        command = 'conda install -y fastqc'
+        run(command, shell=True)
+    def run(self, input):
+        for file in input:
+            command = 'fastqc input/{} -o output/'.format(file)
+            run(command, shell=True)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    fastqc = FASTQC()
+    
+    if args.args:
+        fastqc.install()
+
+    fastqc.run(args.input)
