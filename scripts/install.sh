1577857dab48499530881b8d755fad228d6490aa
Raony Guimaraes <raonyguimaraes@gmail.com>
2019-02-13 22:28:25 +0000

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,27 +16,27 @@ DEB_PACKAGE_NAME="python3 python3-dev python3-pip libssl-dev git"
     yum install -y $YUM_PACKAGE_NAME
  elif cat /etc/*release | grep ^NAME | grep Fedora; then
     echo "================================================"
-    echo "Installing packages $YUM_PACKAGE_NAME on Fedorea"
+    echo "Installing packages $YUM_PACKAGE_NAME on Fedora"
     echo "================================================"
     yum install -y $YUM_PACKAGE_NAME
  elif cat /etc/*release | grep ^NAME | grep Ubuntu; then
     echo "==============================================="
     echo "Installing packages $DEB_PACKAGE_NAME on Ubuntu"
     echo "==============================================="
-    apt-get update
-    apt-get install -y $DEB_PACKAGE_NAME
+    sudo apt-get update
+    sudo apt-get install -y $DEB_PACKAGE_NAME
  elif cat /etc/*release | grep ^NAME | grep Debian ; then
     echo "==============================================="
     echo "Installing packages $DEB_PACKAGE_NAME on Debian"
     echo "==============================================="
-    apt-get update
-    apt-get install -y $DEB_PACKAGE_NAME
+    sudo apt-get update
+    sudo apt-get install -y $DEB_PACKAGE_NAME
  elif cat /etc/*release | grep ^NAME | grep Mint ; then
     echo "============================================="
     echo "Installing packages $DEB_PACKAGE_NAME on Mint"
     echo "============================================="
-    apt-get update
-    apt-get install -y $DEB_PACKAGE_NAME
+    sudo apt-get update
+    sudo apt-get install -y $DEB_PACKAGE_NAME
  elif cat /etc/*release | grep ^NAME | grep Knoppix ; then
     echo "================================================="
     echo "Installing packages $DEB_PACKAGE_NAME on Kanoppix"
