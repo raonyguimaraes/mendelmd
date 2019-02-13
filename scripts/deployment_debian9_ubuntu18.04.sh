d5753d1d60c3de9bd34c6b20e8add50f8b730db4
Raony Guimaraes <raonyguimaraes@gmail.com>
2019-02-13 22:59:05 +0000

    Update deployment_debian9_ubuntu18.04.sh

--- scripts/deployment_debian9_ubuntu18.04.sh
+++ scripts/deployment_debian9_ubuntu18.04.sh
@@ -14,7 +14,7 @@ cd mendelmd/
 
 #set up database
 sudo apt-get install -y libpq-dev postgresql
-echo "create user $USER password ''; ALTER USER $USER WITH SUPERUSER;" > /tmp/create_user.sql
+echo "create user $USER password ''; CREATE ROLE $USER superuser; ALTER ROLE $USER WITH LOGIN;" > /tmp/create_user.sql
 sudo -u postgres psql --file=/tmp/create_user.sql
 createdb mendelmd
 cp mendelmd/local_settings.sample.py mendelmd/local_settings.py
