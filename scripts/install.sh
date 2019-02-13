7eb0961429c898426a546f6ca53eb58005ca34d6
Raony Guimaraes <raonyguimaraes@gmail.com>
2019-02-13 22:24:07 +0000

    Simplify installation

---
+++ scripts/install.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#!/bin/bash
+set -e
+
+YUM_PACKAGE_NAME="python3.6 python3.6-devel python3.6-pip openssl-devel git"
+DEB_PACKAGE_NAME="python3 python3-dev python3-pip libssl-dev git"
+
+ if cat /etc/*release | grep ^NAME | grep CentOS; then
+    echo "==============================================="
+    echo "Installing packages $YUM_PACKAGE_NAME on CentOS"
+    echo "==============================================="
+    yum install -y $YUM_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Red; then
+    echo "==============================================="
+    echo "Installing packages $YUM_PACKAGE_NAME on RedHat"
+    echo "==============================================="
+    yum install -y $YUM_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Fedora; then
+    echo "================================================"
+    echo "Installing packages $YUM_PACKAGE_NAME on Fedorea"
+    echo "================================================"
+    yum install -y $YUM_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Ubuntu; then
+    echo "==============================================="
+    echo "Installing packages $DEB_PACKAGE_NAME on Ubuntu"
+    echo "==============================================="
+    apt-get update
+    apt-get install -y $DEB_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Debian ; then
+    echo "==============================================="
+    echo "Installing packages $DEB_PACKAGE_NAME on Debian"
+    echo "==============================================="
+    apt-get update
+    apt-get install -y $DEB_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Mint ; then
+    echo "============================================="
+    echo "Installing packages $DEB_PACKAGE_NAME on Mint"
+    echo "============================================="
+    apt-get update
+    apt-get install -y $DEB_PACKAGE_NAME
+ elif cat /etc/*release | grep ^NAME | grep Knoppix ; then
+    echo "================================================="
+    echo "Installing packages $DEB_PACKAGE_NAME on Kanoppix"
+    echo "================================================="
+    apt-get update
+    apt-get install -y $DEB_PACKAGE_NAME
+ else
+    echo "OS NOT DETECTED, couldn't install package $PACKAGE"
+    exit 1;
+ fi
+
+exit 0
