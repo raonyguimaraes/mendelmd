7f8180132c087ecd6096d63359feb98ba51e3ed0
Raony Guimaraes <raonyguimaraes@gmail.com>
2021-12-24 00:35:56 +0000

    updates before merge

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,24 +3,23 @@
 service rabbitmq-server start
 sleep 5
 pwd
-cp configs/settings.py /usr/local/lib/python3.6/dist-packages/pynnotator/
+cp configs/settings.py /usr/local/lib/python3.*/dist-packages/pynnotator/
 
 cp mendelmd/local_settings.docker.py mendelmd/local_settings.py
 
 # Collect static files
 echo "Collect static files"
-python3.6 manage.py collectstatic --noinput
+python3 manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Apply database migrations"#
-python3.6 manage.py migrate
+python3 manage.py migrate
 
 #hack to load the genes and diseases only once
 if [ ! -f data/omim/loaded.txt ]; then
-    python3.6 manage.py populate
+    python3 manage.py populate
     touch data/omim/loaded.txt
 fi
-
 
 export C_FORCE_ROOT='true'
 # Start server
@@ -31,4 +30,4 @@ echo "Starting annotator"
 
 # Start server
 echo "Starting server"
-python3.6 manage.py runserver 0.0.0.0:8000
+python3 manage.py runserver 0.0.0.0:8000
