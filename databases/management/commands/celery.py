09f802135e4c01c28a0044509ba41053bea77270
Raony Guimaraes <raony.guimaraes@genuitysci.com>
2023-09-27 17:10:22 +0100

    fix celery

--- databases/management/commands/celery.py
+++ databases/management/commands/celery.py
@@ -4,11 +4,10 @@ import subprocess
 from django.core.management.base import BaseCommand
 from django.utils import autoreload
 
-
 def restart_celery():
     cmd = 'pkill celery'
     subprocess.call(shlex.split(cmd))
-    cmd = 'celery worker -l info -A mendelmd -c 2' #-P solo
+    cmd = 'celery -A mendelmd worker -l info -c 2' #-P solo
     subprocess.call(shlex.split(cmd))
 
 
@@ -16,4 +15,4 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         print('Starting celery worker with autoreload...')
-        autoreload.main(restart_celery)
+        autoreload.run_with_reloader(restart_celery) 
